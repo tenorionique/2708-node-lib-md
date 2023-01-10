b7f9de31b6cdf43510258b8f7a1970262f8038d7
tenorionique <monique_doretto@hotmail.com>
2023-01-10 11:38:47 -0300

    aula 3: expressões regulares e capturando grupos

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
- import fs from 'fs';
- import chalk from 'chalk';
+import fs from 'fs';
+import chalk from 'chalk';
 
- function trataErro(erro) {
+const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+function extraiLinks(texto) {
+    const regex =/\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = regex.exec(texto)
+    console.log(capturas)
+};
+extraiLinks(textoTeste);
+
+function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
- };
-
-// async/await
+};
 
 async function pegaArquivo(caminhoDoArquivo) {
     try {
@@ -16,20 +23,7 @@ async function pegaArquivo(caminhoDoArquivo) {
     catch (erro) {
         trataErro(erro)
     } 
-/*     finally {
-        console.log(chalk.yellow('operação concluída'));
-    } */
-
 }
 
-// promises com then()
-/*  function pegaArquivo(caminhoDoArquivo) {
-    const enconding = 'utf-8';
-    fs.promises
-        .readFile(caminhoDoArquivo, enconding)
-        .then((texto) => console.log(chalk.green(texto)))
-        .catch(trataErro)
- }; */
-
-pegaArquivo('./arquivos/texto.md');
-pegaArquivo('./arquivos/');
+/* pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/'); */
